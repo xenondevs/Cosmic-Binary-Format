b5e6c86cf52a640ed3bf9b5a403ef8238b40b7f9
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-02-17 17:57:21 +0100

    Add option to serialize ordinal enum instead of name for specific enum types

--- Cosmic-Binary-Format/src/test/kotlin/xyz/xenondevs/cbf/adapter/BinaryAdapterTest.kt
+++ Cosmic-Binary-Format/src/test/kotlin/xyz/xenondevs/cbf/adapter/BinaryAdapterTest.kt
@@ -8,22 +8,37 @@ import kotlin.reflect.typeOf
 
 abstract class BinaryAdapterTest<T : Any>(val adapter: BinaryAdapter<T>) {
     
-    inline fun <reified E : T> reserializeValue(value: E): E {
+    inline fun <reified E : T> serializeValue(value: E): ByteArray {
         val type = typeOf<E>()
         
         val out = ByteArrayOutputStream()
         val writer = ByteWriter.fromStream(out)
         adapter.write(value, type, writer)
+        return out.toByteArray()
+    }
+    
+    inline fun <reified E : T> deserializeValue(bin: ByteArray): E {
+        val type = typeOf<E>()
         
-        val ins = ByteArrayInputStream(out.toByteArray())
+        val ins = ByteArrayInputStream(bin)
         val reader = ByteReader.fromStream(ins)
-        
         return adapter.read(type, reader) as E
+    }
+    
+    inline fun <reified E : T> reserializeValue(value: E): E {
+        return deserializeValue(serializeValue(value))
     }
     
     inline fun <reified E : T> copyValue(value: E): E {
         val type = typeOf<E>()
         return adapter.copy(value, type) as E
+    }
+    
+    fun byteWriter(write: ByteWriter.() -> Unit): ByteArray {
+        val out = ByteArrayOutputStream()
+        val writer = ByteWriter.fromStream(out)
+        writer.write()
+        return out.toByteArray()
     }
     
 }
--- Cosmic-Binary-Format/src/test/kotlin/xyz/xenondevs/cbf/adapter/impl/EnumBinaryAdapterTest.kt
+++ Cosmic-Binary-Format/src/test/kotlin/xyz/xenondevs/cbf/adapter/impl/EnumBinaryAdapterTest.kt
@@ -1,16 +1,34 @@
 package xyz.xenondevs.cbf.adapter.impl
 
 import org.junit.jupiter.api.Test
+import xyz.xenondevs.cbf.CBF
 import xyz.xenondevs.cbf.adapter.BinaryAdapterTest
 import java.util.concurrent.TimeUnit
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class EnumBinaryAdapterTest : BinaryAdapterTest<Enum<*>>(EnumBinaryAdapter) {
     
     @Test
-    fun testEnum() {
+    fun testEnumStringBased() {
         val value = TimeUnit.DAYS
-        assertEquals(value, reserializeValue(value))
+        val bin = CBF.write(value)
+        assert(String(bin).contains(value.toString()))
+        assertEquals(value, CBF.read(bin))
+    }
+    
+    @Test
+    fun testEnumOrdinalBased() {
+        EnumBinaryAdapter.addOrdinalEnums(TimeUnit::class)
+        val value = TimeUnit.DAYS
+        val bin = CBF.write(value)
+        
+        assertContentEquals(bin, byteWriter { 
+            writeUnsignedByte(2U)
+            writeVarInt(value.ordinal)
+        })
+    
+        assertEquals(value, CBF.read(bin))
     }
     
 }
