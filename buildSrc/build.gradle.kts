ea1a3fbb98248e327d3154586ad86b57ceba3c5a
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-03-06 12:58:09 +0100

    Set buildSrc java toolchain version to 21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,3 +13,9 @@ dependencies {
     // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
